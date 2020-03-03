a1aec9739452e1ce668976ac30751378fb7095bb
JaegyuJeong <h923313@gmail.com>
2020-03-03 13:58:09 +0900

    SWEA_D5_1247

--- swea_d5/Solution_1247_최적경로.java
+++ swea_d5/Solution_1247_최적경로.java
@@ -1,101 +1,64 @@
-package swea.d5;
+package swea_d5;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-class Company {
-	int x;
-	int y;
+class Pair {
+    int x, y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
+public class Solution_1247_최적경로{
+    static int customer;
+    static int result;
+    static int min;
+    static Pair[] arr;
+    static int[] check;
 
-class Home {
-	int x;
-	int y;
-}
+    public static void main(String[] args) {
 
-class Customer {
-	int x;
-	int y;
-	boolean visit;
-}
+        Scanner sc = new Scanner(System.in);
+        int testcase = sc.nextInt();
+        for (int t = 1; t <= testcase; t++) {
+            result = 0;
+            min = Integer.MAX_VALUE;
+            customer = sc.nextInt();
+            arr = new Pair[customer + 2];
+            check = new int[customer + 2];
+            arr[0] = new Pair(sc.nextInt(), sc.nextInt()); // start
+            arr[customer + 1] = new Pair(sc.nextInt(), sc.nextInt()); // end
+            for (int i = 1; i <= customer; i++) {
+                arr[i] = new Pair(sc.nextInt(), sc.nextInt());
+            }
+            check[0] = 1;
+            find(0, 0, 0);
 
-public class Solution_1247_최적경로 {
-	static int total_distance; // 총 이동거리
-	static int temp_distance; // 임시 이동거리 (백트래킹)
-	static Company company;
-	static Home home;
-	static Customer[] customer;
-	static int customer_su;
+            System.out.println("#" + t + " " + min);
+        }
 
-	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(br.readLine());
-			company = new Company();
-			home = new Home();
-			customer = new Customer[N];
-			customer_su = N - 2;
+    }
 
-//			입력
-			StringTokenizer st = new StringTokenizer(br.readLine());
+    private static void find(int index, int depth, int result) {
+        if(min<=result) {    //여기 시간 줄이기에 제일 중요
+            return;
+        }
+        if (depth == customer) {
+            result += Math.abs(arr[index].x - arr[customer + 1].x) + Math.abs(arr[index].y - arr[customer + 1].y);
+            if (min > result) {
+                min = result;
+            }
+        }
 
-//			 회사
-			company.x = Integer.parseInt(st.nextToken());
-			company.y = Integer.parseInt(st.nextToken());
-//			 집
-			home.x = Integer.parseInt(st.nextToken());
-			home.y = Integer.parseInt(st.nextToken());
-//			 고객 위치
-			for (int i = 0; i < customer_su; i++) {
-				customer[i] = new Customer();
-				customer[i].x = Integer.parseInt(st.nextToken());
-				customer[i].y = Integer.parseInt(st.nextToken());
-				customer[i].visit = false;
-			}
+        for (int i = 1; i <= customer; i++) {
+            if (check[i] == 0) {
+                // result = Math.abs(arr[index].x- arr[i].x)+ Math.abs(arr[index].y- arr[i].y);
+                check[i] = 1;
+                find(i, depth + 1, result + Math.abs(arr[index].x - arr[i].x) + Math.abs(arr[index].y - arr[i].y));
+                check[i] = 0;
 
-//			경로 찾기
-			temp_distance = 0;
-			dfs(temp_distance, 0);
-
-		}
-	}
-
-	private static void dfs(int temp_distance, int cnt) {
-		if (cnt == customer_su) {
-			total_distance += calc(home.x, home.y, customer[cnt].x, customer[cnt].y);
-		}
-
-		if (cnt == 0) {
-			for (int i = 0; i < customer_su; i++) {
-				temp_distance = calc(company.x, company.y, customer[i].x, customer[i].y);
-				customer[i].visit = true;
-				dfs(temp_distance, cnt + 1);
-				customer[i].visit = false;
-			}
-		}
-
-		for (int i = 0; i < customer_su - 1; i++) {
-			for (int j = i; j < customer_su; j++) {
-				if (temp_distance < total_distance && !customer[j].visit) {
-					temp_distance += calc(customer[i].x, customer[i].y, customer[j].x, customer[j].y);
-					customer[i].visit = true;
-					dfs(temp_distance, cnt + 1);
-					customer[i].visit = false;
-				}
-
-			}
-
-		}
-
-	}
-
-	private static int calc(int pre_x, int pre_y, int post_x, int post_y) {
-
-		return Math.abs(pre_x - post_x) + Math.abs(pre_y - post_y);
-	}
+            }
+        }
+    }
 
 }
